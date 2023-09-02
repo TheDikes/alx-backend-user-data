599428af84ce068952bdf30a003c837f9fa27cd1
Maa-mmy <dikecynthia22@gmail.com>
2023-09-02 20:29:39 +0100

    Encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 """
-hashing password
+Encrypting passwords
 """
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
-    """ Returns a salted, hashed password which is a byte string """
-    salt = bcrypt.gensalt();
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    """ Returns a salted, hashed password, which is a byte string """
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
 
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ validating that the provided password matches the hashed password """
-    encoded = password.encode('utf-8')
+    """ Validates the provided password matches the hashed password """
+    valid = False
+    encoded = password.encode()
     if bcrypt.checkpw(encoded, hashed_password):
-        return True
-    else:
-        return False
+        valid = True
+    return valid
